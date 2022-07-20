fcb0c4d60079f843add761cf9d27eb484434316d
nimit2801 <mnimitsavant@outlook.com>
2022-07-21 02:14:32 +0530

    Created ✨ 1. Bookmark and User route 2. Auth folder 3. dotenv 4. docker image for psql 5. connected the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
+
